eefbd188016dec03674f57b4e2a6c2ded8d5aa93
Rodrigo Mansueli Nunes <mansueli@engineer.com>
2014-06-22 15:41:44 -0600

    Código mais limpo
    limpeza geral

--- Respostas/StackOverflow22135/src/stack/A.java
+++ Respostas/StackOverflow22135/src/stack/A.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package stack;
 
@@ -12,10 +7,6 @@ package stack;
  */
 public class A {
        public void getClasse(){
-           try{
                System.out.println("class = " + this.getClass());
-           }catch(Exception e){
-           
-           }
        }
 }
