a8bf134896c7c8d7565558d38c607dbb884a5601
Rodrigo Mansueli Nunes <mansueli@engineer.com>
2014-06-22 15:31:28 -0600

    Resposta a pergunta 22135

---
+++ Respostas/StackOverflow22135/src/stack/A.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package stack;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class A {
+       public void getClasse(){
+           try{
+               System.out.println("class = " + this.getClass());
+           }catch(Exception e){
+           
+           }
+       }
+}
